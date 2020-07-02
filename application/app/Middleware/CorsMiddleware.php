eedd64795da0cf6b84be95e96c099617e613c1ce
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-07-02 06:27:17 -0300

    feat: adiciona CorsMiddleware

---
+++ application/app/Middleware/CorsMiddleware.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * Vagner Cardoso <https://github.com/vagnercardosoweb>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 02/07/2020 Vagner Cardoso
+ */
+
+namespace App\Middleware;
+
+use Fig\Http\Message\StatusCodeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteContext;
+
+/**
+ * Class CorsMiddleware.
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ */
+class CorsMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $routeContext = RouteContext::fromRequest($request);
+        $routingResults = $routeContext->getRoutingResults();
+        $allowedMethods = $routingResults->getAllowedMethods();
+        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+        $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $allowedMethods));
+        $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+        // Allow Ajax CORS requests with Authorization header
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+        if ('options' === strtolower($request->getMethod())) {
+            $response = $response->withStatus(StatusCodeInterface::STATUS_OK);
+        }
+
+        return $response;
+    }
+}
