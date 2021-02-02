ec3bbf75b95499a559222df1dbbee9acf0a198b4
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-02-02 19:12:39 -0300

    style: run cs:fix
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * Vagner Cardoso <https://github.com/vagnercardosoweb>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 02/02/2021 Vagner Cardoso
+ */
+
 require_once __DIR__.'/../vendor/autoload.php';
