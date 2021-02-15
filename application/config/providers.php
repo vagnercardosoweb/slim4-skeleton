baa7e80b0e934c1696afb707746d250665271222
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-02-15 02:39:14 -0300

    style: run cs:fix
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- application/config/providers.php
+++ application/config/providers.php
@@ -6,7 +6,7 @@
  * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
  * @link https://github.com/vagnercardosoweb
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright 11/02/2021 Vagner Cardoso
+ * @copyright 15/02/2021 Vagner Cardoso
  */
 
 use App\Providers\DatabaseProvider;
