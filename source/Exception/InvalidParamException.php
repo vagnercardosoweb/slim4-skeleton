2eaa3fdd2ebae180a0ecceafdc0015cefa585e60
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-02-24 03:07:11 -0300

    feat: erro para parâmetro inválido
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

---
+++ source/Exception/InvalidParamException.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Vagner Cardoso <https://github.com/vagnercardosoweb>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 24/02/2021 Vagner Cardoso
+ */
+
+namespace Core\Exception;
+
+use Fig\Http\Message\StatusCodeInterface;
+
+/**
+ * Class InvalidParamException.
+ */
+class InvalidParamException extends \InvalidArgumentException
+{
+    /**
+     * InvalidParamException constructor.
+     *
+     * @param string $param
+     */
+    public function __construct(string $param)
+    {
+        parent::__construct(
+            "Invalid parameters on request: {$param}",
+            StatusCodeInterface::STATUS_BAD_REQUEST,
+            $this->getPrevious()
+        );
+    }
+}
