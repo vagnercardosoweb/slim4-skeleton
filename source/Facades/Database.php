7d00a7a7c979e20bce856545375ad8ec09679352
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-03-16 00:52:18 -0300

    feat: adiciona os métodos na facade database
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

--- source/Facades/Database.php
+++ source/Facades/Database.php
@@ -6,15 +6,30 @@
  * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
  * @link https://github.com/vagnercardosoweb
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright 01/03/2021 Vagner Cardoso
+ * @copyright 16/03/2021 Vagner Cardoso
  */
 
 namespace Core\Facades;
 
+use Core\EventEmitter;
+
 /**
  * Class Database.
  *
- * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @method static void setEventEmitter(?EventEmitter $event)
+ * @method static \Core\Database\Database addConnection(string $driver, array $connection)
+ * @method static \Core\Database\Database driver(?string $driver = null)
+ * @method static \Core\Database\Database connection(?string $driver = null)
+ * @method static string getDefaultDriver()
+ * @method static \Core\Database\Database setDefaultDriver(string $defaultDriver)
+ * @method static \PDO getPdo()
+ * @method static array getConnections()
+ * @method static mixed transaction(\Closure $callback)
+ * @method static ?int create(string $table, object | array $data)
+ * @method static \Core\Database\Connection\Statement query(string $sql, $bindings = null, array $driverOptions = [])
+ * @method static ?array update(string $table, object | array $data, string $condition, $bindings = null)
+ * @method static \Core\Database\Connection\Statement read(string $table, ?string $condition = null, $bindings = null)
+ * @method static ?array delete(string $table, string $condition, $bindings = null)
  */
 class Database extends Facade
 {
