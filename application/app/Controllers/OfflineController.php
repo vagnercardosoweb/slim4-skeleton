26c3393bae0dfb78dc1e40732236734b91016550
Vagner Cardoso <vagnercardosoweb@gmail.com>
2021-05-07 19:20:03 -0300

    feat: adiciona página offline
    Signed-off-by: Vagner Cardoso <vagnercardosoweb@gmail.com>

---
+++ application/app/Controllers/OfflineController.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Vagner Cardoso <https://github.com/vagnercardosoweb>
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ * @link https://github.com/vagnercardosoweb
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright 07/05/2021 Vagner Cardoso
+ */
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class OfflineController.
+ *
+ * @author Vagner Cardoso <vagnercardosoweb@gmail.com>
+ */
+class OfflineController extends BaseController
+{
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function index(): ResponseInterface
+    {
+        return $this->withTwig('offline');
+    }
+}
